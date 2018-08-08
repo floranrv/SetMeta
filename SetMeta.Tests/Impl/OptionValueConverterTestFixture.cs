e4f77dcb92307c1b9f72f2fbf5e9b3b60eb5d496
floranRV <floranRV@gmail.com>
2018-08-08 10:25:17 +0300

    Implemented OptionValueConverterTestFixture, added AssemblyInfo in order to have access to internal class OptionValueConverter

--- SetMeta.Tests/Impl/OptionValueConverterTestFixture.cs
+++ SetMeta.Tests/Impl/OptionValueConverterTestFixture.cs
@@ -1,10 +1,46 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.ComponentModel;
+using NUnit.Framework;
+using SetMeta.Abstract;
+using SetMeta.Impl;
 
 namespace SetMeta.Tests.Impl
 {
     [TestFixture]
-    public class OptionValueConverterTestFixture
+    internal class OptionValueConverterTestFixture
+        : SutBase<OptionValueConverter<string>, IOptionValueConverter<string>>
     {
-        
+        [Test]
+        public void GetValue_WhenWePassString_ThereShouldBeNoException()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var actual = Sut.GetValue(Fake<string>());
+
+                Assert.IsNotNull(actual);
+            });
+        }
+
+        [Test]
+        public void GetStringValue_WhenWePassString_ThereShouldBeNoException()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var actual = Sut.GetStringValue(Fake<string>());
+
+                Assert.IsNotNull(actual);
+            });
+        }
+
+        [Test]
+        public void GetStringValue_WhenFormatProviderIsNull_ThrowException()
+        {
+            var ex = Assert.Throws<ArgumentNullException>(() =>
+            {
+                Sut.GetStringValue(Fake<string>(), null);
+            });
+
+            Assert.That(ex.ParamName, Is.EqualTo("formatProvider"));
+        }
     }
 }
