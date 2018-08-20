ad550fe03f29fcc71368f248e7eb053ceb109938
floranRV <floranRV@gmail.com>
2018-08-20 20:04:20 +0300

    T-008 Added MultiListOptionBehaviour and tests for it

---
+++ SetMeta/Behaviours/MultiListOptionBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using SetMeta.Abstract;
+using SetMeta.Entities;
+
+namespace SetMeta.Behaviours
+{
+    public class MultiListOptionBehaviour
+        : OptionBehaviour
+    {
+        internal MultiListOptionBehaviour(IOptionValue optionValue, IEnumerable<ListItem> validItems, bool sorted = false, string separator = ";")
+            : base(optionValue)
+        {
+            ListItems = new List<ListItem>(validItems);
+            Sorted = sorted;
+            Separator = separator;
+        }
+
+        public List<ListItem> ListItems { get; private set; }
+        public bool Sorted { get; private set; }
+        public string Separator { get; private set; }
+
+    }
+}
